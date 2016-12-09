637cbbc1f4e10b5104a31cdd2e6251a22e8294e8
RTC18 <rtc18@rtc18de-imac.local>
2016-12-09 15:10:43 +0800

    Initial Commit

---
+++ FCUMAP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FCUMAP
+//
+//  Created by RTC18 on 2016/12/9.
+//  Copyright © 2016年 AhKui-D0562215. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
